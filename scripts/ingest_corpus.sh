10442246dab1b2ae683706765c7a40f27fb9135d
Sabine <ssegaloff18@gmail.com>
2026-04-08 18:54:15 -0400

    added --depth 1 to git clone calls in script

--- scripts/ingest_corpus.sh
+++ scripts/ingest_corpus.sh
@@ -5,17 +5,17 @@ mkdir raw_data
 cd raw_data
 
 echo "Cloning repositories..."
-git clone git@github.com:standardebooks/agatha-christie_the-murder-at-the-vicarage.git
-git clone git@github.com:standardebooks/agatha-christie_giants-bread.git
-git clone git@github.com:standardebooks/agatha-christie_the-secret-adversary.git
-git clone git@github.com:standardebooks/agatha-christie_the-murder-on-the-links.git
-git clone git@github.com:standardebooks/agatha-christie_the-man-in-the-brown-suit.git
-git clone git@github.com:standardebooks/agatha-christie_the-big-four.git
-git clone git@github.com:standardebooks/agatha-christie_poirot-investigates.git
-git clone git@github.com:standardebooks/agatha-christie_the-mysterious-affair-at-styles.git
-git clone git@github.com:standardebooks/agatha-christie_the-secret-of-chimneys.git
-git clone git@github.com:standardebooks/agatha-christie_the-seven-dials-mystery.git
-git clone git@github.com:standardebooks/agatha-christie_the-murder-of-roger-ackroyd.git
-git clone git@github.com:standardebooks/agatha-christie_the-mystery-of-the-blue-train.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-murder-at-the-vicarage.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_giants-bread.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-secret-adversary.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-murder-on-the-links.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-man-in-the-brown-suit.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-big-four.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_poirot-investigates.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-mysterious-affair-at-styles.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-secret-of-chimneys.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-seven-dials-mystery.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-murder-of-roger-ackroyd.git
+git clone --depth 1 git@github.com:standardebooks/agatha-christie_the-mystery-of-the-blue-train.git
 
 echo "Ingestion complete."
